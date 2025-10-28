eced8ae1a3f2078d7c4687da0ee1866c351452b2
parkjunhwa <junhwa.park@gmail.com>
2025-10-28 14:14:48 +0900

    관리 페이지 오타 수정완료

--- src/app/published/adm004/page.tsx
+++ src/app/published/adm004/page.tsx
@@ -21,7 +21,7 @@ export default function InterfaceLogDetailPage() {
       {/* Breadcrumb and Page Title */}
       <div className="flex flex-row items-center justify-between mt-1 mb-3">
         <div>
-          <h1 className="text-2xl font-bold text-gray-900">I/F로그 관라</h1>
+          <h1 className="text-2xl font-bold text-gray-900">I/F로그 관리</h1>
         </div>
         <div>
           <Breadcrumb
